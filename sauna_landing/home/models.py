693f17c13c412aac0c9e3632cc38c03734e344c4
alex <a556077@icloud.com>
2024-11-03 00:07:47 +0300

    dinamic blocks

--- sauna_landing/home/models.py
+++ sauna_landing/home/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, InlinePanel
-from wagtail.contrib.settings.models import register_setting
 from modelcluster.fields import ParentalKey
-from modelcluster.models import ClusterableModel
 
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.blocks import StructBlock, StreamBlock, RichTextBlock
+from wagtail.fields import StreamField
+from wagtail.admin.panels import FieldPanel
+from django.db import models
+from wagtail.models import Page
 
 class Service(Orderable):
     page = ParentalKey("HomePage", on_delete=models.CASCADE, related_name="services")
@@ -22,11 +27,65 @@ class Service(Orderable):
     def __str__(self):
         return self.name
 
+    class FAQ(Orderable):
+        page = ParentalKey("HomePage", on_delete=models.CASCADE, related_name="faqs")
+        question = models.CharField(max_length=255)
+        answer = RichTextField(blank=True)
+
+        panels = [
+            FieldPanel("question"),
+            FieldPanel("answer"),
+        ]
+
+        def __str__(self):
+            return self.question
+
+
+
+
+
+
+
+
+
+#
+# class SaunaDepartmentBlock(blocks.StructBlock):
+#     title = blocks.CharBlock(required=True, max_length=60, label="Название отделения")
+#     description = blocks.RichTextBlock(required=True, label="Описание отделения")
+#     image = ImageChooserBlock(required=True, label="Изображение")
+#
+#     class Meta:
+#         template = "blocks/sauna_department_block.html"
+#         icon = "fa-bath"
+#         label = "Отделение сауны"
+
+
+
+
+class DepartmentBlock(StructBlock):
+    title = blocks.CharBlock(required=True, max_length=60, label="Название отделения")
+    description = RichTextBlock(required=False, verbose_name="Описание отделения")
+    images = StreamBlock([
+        ("image", ImageChooserBlock(required=True))
+    ], verbose_name="Фотографии", required=False)
+
+    class Meta:
+        #template = "blocks/sauna_department_block.html"
+        icon = "folder"
+        label = "Отделение"
+
+
 
 class HomePage(Page):
     body = RichTextField(blank=True)
 
+    departments = StreamField([
+        ('department', DepartmentBlock(required=False)),
+    ], verbose_name="Отделения сауны", use_json_field=True,)
+
     content_panels = Page.content_panels + [
         FieldPanel("body"),
         InlinePanel("services", label="Услуги"),
+        InlinePanel("faqs", label="Вопросы и ответы"),
+        FieldPanel('departments'),
     ]
