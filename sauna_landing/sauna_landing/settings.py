09e4f7ea638a19bd30027cdeb90431f7a2586e93
alex <a556077@icloud.com>
2026-05-24 00:27:51 +0300

    seo optimization

--- sauna_landing/sauna_landing/settings.py
+++ sauna_landing/sauna_landing/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
     }
 }
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
